2fee404a8b78072e81a9240065cb8099681d26ed
Jack Coggins <voidawake90@gmail.com>
2021-02-27 17:03:06 +1100

    Ad Attack scriptable object

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : ScriptableObject
+{
+    public GameObject attackObject;
+    public KeyCode input;
+    public KeyCode modifier;
+}
