cd95f5088674c348d8ba133342fa488bcf91c8aa
Habibullah Saleem <62326123+ItsRaijin@users.noreply.github.com>
2021-02-27 18:28:50 +1100

    saturday final attack build
    end this already

--- Assets/Scripts/AttackController.cs
+++ Assets/Scripts/AttackController.cs
@@ -38,17 +38,17 @@ public class AttackController : MonoBehaviour
     }
 
     private void attackNormal(){
-        Vector3 offset = new Vector3(-0.9f,-4.5f, 0f);
+        Vector3 offset = new Vector3(-1f,0, 0f);
         Instantiate(normalAttack, transform.position + offset, Quaternion.identity);
     }
 
     private void attackUp(){
-        Vector3 offset = new Vector3(0f,-3.7f, 0f);
+        Vector3 offset = new Vector3(0f,1, 0f);
         Instantiate(verticalAttack, transform.position + offset, Quaternion.identity);
     }
 
     private void attackDown(){
-        Vector3 offset = new Vector3(0f,-4.8f, 0f);
+        Vector3 offset = new Vector3(0f,-1, 0f);
         Instantiate(verticalAttack, transform.position + offset, Quaternion.identity);
     }
 
