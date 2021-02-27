ea690d826ad1ab856116ffaf126be9db0ee33561
HugoA-slash-UsernamePassword <hugoa3520@gmail.com>
2021-02-28 09:58:05 +1100

    enemy stuff not that much n\ore

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -6,7 +6,9 @@ public class EnemyManager : MonoBehaviour
 {
     [SerializeField] public GameObject fire;
     public phase curPhase;
-    private int cooldown;
+    private float cooldown;
+    private int attPattern;
+    public Transform player;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,21 +18,35 @@ public class EnemyManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        cooldown++;
+        cooldown += Time.deltaTime;
         switch (curPhase)
         {
             case phase.idle:
+                if (AttackDelay(1))
+                {
+                    curPhase = (phase)Random.Range(1, 3);
+                }
                 break;
             case phase.fireball:
-                if (AttackDelay(40))
+                if (AttackDelay(0.2f))
                 {
                     Instantiate(fire);
+                    if (attPattern == 5)
+                    {
+                        attPattern = 0;
+                        curPhase = phase.idle;
+                    }
                 }
                 break;
             case phase.attack:
-                if (cooldown > 40)
+                if (AttackDelay(2))
                 {
-
+                    transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, Mathf.Sign(transform.position.x - player.position.x));
+                    if(attPattern == 2)
+                    {
+                        attPattern = 0;
+                        curPhase = phase.idle;
+                    }
                 }
                 break;
             case phase.help:
@@ -40,10 +56,11 @@ public class EnemyManager : MonoBehaviour
         }
     }
 
-    private bool AttackDelay(int delay)
+    private bool AttackDelay(float delay)
     {
         if(cooldown > delay)
         {
+            attPattern++;
             cooldown = 0;
             return true;
         }
