b642e424c23918a7ba0687711f9ad142f7fb27cc
HugoA-slash-UsernamePassword <hugoa3520@gmail.com>
2021-02-27 15:35:40 +1100

    helth

---
+++ Assets/Health.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Image hearts;
+    public Text enemyText;
+
+    private int curHP;
+    public int maxHP;
+    private int bossHP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curHP = maxHP;
+        UpdateUI();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+
+    }
+    public void UpdateUI()
+    {
+        hearts.rectTransform.sizeDelta = new Vector2(50 * curHP, 50);
+        if (curHP == 0) Die();
+
+    }
+    public void DamageEnemy(int att)
+    {
+        bossHP -= att;
+        //enemyText.text = enemyHP;
+    }
+
+    public void DamagePlayer(int att)
+    {
+        curHP -= att;
+        curHP = Mathf.Clamp(curHP, 0, maxHP);
+        UpdateUI();
+        //enemyText.text = enemyHP;
+    }
+    private void Die()
+    {
+        Debug.LogError("You have died. Please exit the game.");
+    }
+}
