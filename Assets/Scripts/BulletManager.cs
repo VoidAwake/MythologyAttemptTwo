8013f0f51c913f24ca6cc40bd6b80d545fcd2932
Habibullah Saleem <62326123+ItsRaijin@users.noreply.github.com>
2021-02-28 17:22:38 +1100

    some assets
    adfwfwfaf

---
+++ Assets/Scripts/BulletManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject Player;
+    [SerializeField] private GameObject Bullet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(LevelManager.roundCount > 4){
+            Fire();
+        }
+    }
+
+    private void Fire(){
+
+    }
+}
