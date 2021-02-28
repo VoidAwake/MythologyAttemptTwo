89f856ad709b350297023dd98aeaf799793fc063
HugoA-slash-UsernamePassword <hugoa3520@gmail.com>
2021-02-28 14:59:21 +1100

    yeah sure

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -10,6 +10,8 @@ public class LevelManager : MonoBehaviour
     private float timer;
     public static int level;
     public GridSetup grid;
+    public static int maxScore;
+    public static int currentScore;
     // Start is called before the first frame update
     void Start()
     {
