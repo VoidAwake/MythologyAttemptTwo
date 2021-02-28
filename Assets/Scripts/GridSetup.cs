766fc5e71dcee93a049ba6be17e17e10739fa482
HugoA-slash-UsernamePassword <hugoa3520@gmail.com>
2021-02-28 12:41:07 +1100

    poosh

--- Assets/Scripts/GridSetup.cs
+++ Assets/Scripts/GridSetup.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid : MonoBehaviour
+public class GridSetup : MonoBehaviour
 {
     public GameObject black, white;
     public Texture2D[] maps;
@@ -10,12 +10,14 @@ public class Grid : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int i = 0;
+
+        int i = Random.Range(0, maps.Length);
+
         for (int x = 0; x < 40; x++)
         {
             for (int y = 0; y < 40; y++)
             {
-                Instantiate(maps[i].GetPixel(x, y) == Color.black ? black : white, new Vector2(topRight.position.x + x * 0.25f, topRight.position.y + y * 0.25f), Quaternion.identity);
+                Instantiate(maps[i].GetPixel(x, y) == Color.black ? black : white, new Vector2(topRight.position.x + x * 0.25f, topRight.position.y + y * 0.25f), Quaternion.identity, transform);
             }
         }
     }
